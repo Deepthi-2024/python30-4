554636a8a926d11ba12b12d1755ec117d6be6b2f
Deepthi <dagududeepthi@gmail.com>
2023-04-30 15:43:10 +0530

    second

--- one.py
+++ one.py
@@ -2,6 +2,6 @@ str=input("Enter a string : ")
 str1 = []
 for i in str:
     if i not in str1:
-        str1.apprnd(i)
+        str1.append(i)
 for i in range(0,len(str1)):
-    print(i,end=",")
+    print(str1[i],end=",")
